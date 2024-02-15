30c8296c87886d30733355c8dd70b7ee90d0de4c
Siva Sankar Reddy Veluri <veluri.sivasankar@gmail.com>
2024-02-15 16:24:12 -0500

    correct resources path and call map

--- src/test/java/game/map/MapSaverTest.java
+++ src/test/java/game/map/MapSaverTest.java
@@ -38,14 +38,14 @@ public class MapSaverTest {
     }
 
     private void linkCountries() {
-        countries.get(0).getNeighbours().addAll(List.of(1, 2, 4));
-        countries.get(1).getNeighbours().addAll(List.of(0, 2));
-        countries.get(2).getNeighbours().addAll(List.of(3, 4, 0));
-        countries.get(3).getNeighbours().addAll(List.of(1, 5));
-        countries.get(4).getNeighbours().addAll(List.of(0, 5));
-        countries.get(5).getNeighbours().addAll(List.of(0, 1));
-        countries.get(6).getNeighbours().addAll(List.of(7, 2));
-        countries.get(7).getNeighbours().addAll(List.of(6, 2, 5));
+        countries.get(0).getNeighbors().addAll(List.of(1, 2, 4));
+        countries.get(1).getNeighbors().addAll(List.of(0, 2));
+        countries.get(2).getNeighbors().addAll(List.of(3, 4, 0));
+        countries.get(3).getNeighbors().addAll(List.of(1, 5));
+        countries.get(4).getNeighbors().addAll(List.of(0, 5));
+        countries.get(5).getNeighbors().addAll(List.of(0, 1));
+        countries.get(6).getNeighbors().addAll(List.of(7, 2));
+        countries.get(7).getNeighbors().addAll(List.of(6, 2, 5));
     }
 
     @AfterEach
--- src/test/java/game/pojo/CountryTest.java
+++ src/test/java/game/pojo/CountryTest.java
@@ -27,7 +27,7 @@ class CountryTest {
     }
 
     @Test
-    void getNeighbours() {
+    void getNeighbors() {
     }
 
     @Test
