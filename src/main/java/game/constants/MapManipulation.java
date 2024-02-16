bf44231357a4013b3cd88b3bb8d53c383bba90ed
Joyjit Choudhury <joyjitc@users.noreply.github.com>
2024-02-15 20:41:32 -0500

    Added MapManipulation Constants

---
+++ src/main/java/game/constants/MapManipulation.java
@@ -0,0 +1,18 @@
+package game.constants;
+
+/**
+ * This class contains the commands and their parameters
+ * used to manipulate maps, stored as a String
+ */
+public class MapManipulation {
+
+    public static final String EDIT_CONTINENT = "editcontinent";
+
+    public static final String EDIT_COUNTRY = "editcountry";
+
+    public static final String EDIT_NEIGHBOR = "editneighbor";
+
+    public static final String ADD_PARAM = "-add";
+
+    public static final String REMOVE_PARAM = "-remove";
+}
