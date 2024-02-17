c66610ddd2fe18c48bfa49f3352e3ccc69f6f237
Joyjit Choudhury <joyjitc@users.noreply.github.com>
2024-02-17 14:13:14 -0500

    Added functions to Remove Country/Continent

--- src/main/java/game/map/Map.java
+++ src/main/java/game/map/Map.java
@@ -6,10 +6,7 @@ import game.pojo.Player;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Java Model for the Game Map. All manipulation of the Map is executed on an object of this class
@@ -20,11 +17,10 @@ public class Map {
 
   private List<String> d_comments;
   private List<String> d_files;
-    private final List<Continent> continents;
-    private final List<Country> countries;
-    private final List<Player> players;
+  private final List<Continent> continents;
+  private final List<Country> countries;
+  private final List<Player> players;
   private TreeMap<Integer, TreeSet<Integer>> d_borders;
-
 
   public Map() {
     this(
@@ -36,54 +32,80 @@ public class Map {
         new TreeMap<>());
   }
 
-    public void addContinent(Continent continent) {
+  public void addContinent(Continent continent) {
 
-        for (Continent thisContinent : continents) {
-            if (thisContinent.getId() == continent.getId()) {
-                throw new IllegalArgumentException("Continent with same id already exists");
-            }
-        }
-
-        continents.add(continent);
+    for (Continent thisContinent : continents) {
+      if (thisContinent.getId() == continent.getId()) {
+        throw new IllegalArgumentException("Continent with same id already exists");
+      }
     }
 
-    public void addCountry(Country country) {
+    continents.add(continent);
+  }
 
-        for (Country thisCountry : countries) {
-            if (thisCountry.getId() == country.getId()) {
-                throw new IllegalArgumentException("Country with same id already exists");
-            }
-        }
-        countries.add(country);
+  public void removeContinent(Integer p_continent_id) {
+    Boolean l_is_continent_removed =
+        continents.removeIf(continent -> continent.getId() == p_continent_id);
+
+    if (l_is_continent_removed) {
+      System.out.println("Continent removed successfully!");
+    } else {
+      System.out.println("No Continent with the given ID exists!");
     }
+  }
 
-    public void addPlayer(String playerName) {
+  public void addCountry(Country country) {
 
-        for (Player thisPlayer : players) {
-            if (thisPlayer.getName().equals(playerName)) {
-                throw new IllegalArgumentException("Player with same name already exists");
-            }
-        }
-        players.add(new Player(playerName));
+    for (Country thisCountry : countries) {
+      if (thisCountry.getId() == country.getId()) {
+        throw new IllegalArgumentException("Country with same id already exists");
+      }
     }
+    countries.add(country);
+  }
 
-    public void removePlayer(String playerName) {
+  public void removeCountry(Integer p_country_id) {
+    Boolean l_is_country_removed = countries.removeIf(country -> country.getId() == p_country_id);
 
-        for (Player thisPlayer : players) {
-            if (thisPlayer.getName().equals(playerName)) {
-                players.remove(thisPlayer);
-                return;
-            }
-        }
-        throw new IllegalArgumentException("No player exists with this name");
+    if (l_is_country_removed) {
+      System.out.println("Country removed successfully!");
+    } else {
+      System.out.println("No Country with the given ID exists!");
     }
+  }
 
-    public void addBorder(Integer p_country_1, Integer p_country_2) {
-        if (this.d_borders == null) {
-            this.d_borders = new TreeMap<>();
-        }
-          this.d_borders.computeIfAbsent(p_country_1, k -> new TreeSet<>());
-          this.d_borders.get(p_country_1).add(p_country_2);
+  public void addPlayer(String playerName) {
+
+    for (Player thisPlayer : players) {
+      if (thisPlayer.getName().equals(playerName)) {
+        throw new IllegalArgumentException("Player with same name already exists");
+      }
     }
+    players.add(new Player(playerName));
+  }
 
+  public void removePlayer(String playerName) {
+
+    for (Player thisPlayer : players) {
+      if (thisPlayer.getName().equals(playerName)) {
+        players.remove(thisPlayer);
+        return;
+      }
+    }
+    throw new IllegalArgumentException("No player exists with this name");
+  }
+
+//  public void addNeighborToCountry(Integer p_country_id, Integer p_neighbor_country_id) {
+//    Country country = countries.stream()
+//            .filter(Objects::nonNull)
+//            .
+//  }
+
+  public void addBorder(Integer p_country_1, Integer p_country_2) {
+    if (this.d_borders == null) {
+      this.d_borders = new TreeMap<>();
+    }
+    this.d_borders.computeIfAbsent(p_country_1, k -> new TreeSet<>());
+    this.d_borders.get(p_country_1).add(p_country_2);
+  }
 }
