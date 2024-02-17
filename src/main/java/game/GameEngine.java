a8332eafc65626b4f50aacc6a014812a6337e15a
shanmukhatummala <shanmukha.tummala@outlook.com>
2024-02-17 18:32:49 -0500

    add game loop

--- src/main/java/game/GameEngine.java
+++ src/main/java/game/GameEngine.java
@@ -1,7 +1,6 @@
 package game;
 
 import game.map.Map;
-import game.map.MapShower;
 import game.pojo.Country;
 import game.pojo.Player;
 
@@ -12,6 +11,7 @@ import java.util.List;
 
 import static game.map.MapEditor.editMap;
 import static game.map.MapLoader.loadMap;
+import static game.map.MapShower.showMap;
 import static game.util.FileHelper.createNewFileForMap;
 import static game.util.FileHelper.fileExists;
 
@@ -56,7 +56,7 @@ public class GameEngine {
                     editMap(l_bufferedReader, l_map, l_fileName);
                 }
                 else if (l_commandArgs.length == 1 && "showmap".equals(l_commandArgs[0])) {
-                    MapShower.showMap(l_map);
+                    showMap(l_map);
                 }
                 else if (l_commandArgs.length >= 1 && "gameplayer".equals(l_commandArgs[0])) {
                     if (!isValidGamePlayerCommand(l_commandArgs)) {
@@ -75,12 +75,14 @@ public class GameEngine {
                             System.out.println(e.getMessage());
                         }
                     }
-                } else if (l_commandArgs.length== 1 && l_commandArgs[0].equals("assigncountries")) {
+                } else if (l_commandArgs.length == 1 && l_commandArgs[0].equals("assigncountries")) {
 
                     List<Player> players = l_map.getD_players();
                     List<Country> countries = l_map.getD_countries();
                     l_map.assignCountries(players, countries);
-
+                    startGameLoop(l_map);
+                    System.out.println("Game over - all orders executed");
+                    endGame();
                 } else {
                     throw new IllegalArgumentException("Not a valid command");
                 }
@@ -90,12 +92,54 @@ public class GameEngine {
         }
     }
 
-    public Map getD_map() {
-        return d_map;
+// Actual loop should look like this. But for now we use the below
+//    private void startGameLoop() {
+//        while (true) {
+//            assignReinforcements();
+//            issueOrders();
+//            executeOrders();
+//
+//            if (allCountriesOccupiedByOnePlayer()) {
+//                declareWinner();
+//                exitFromGame();
+//            }
+//        }
+//    }
+
+    private static void startGameLoop(Map p_map) {
+        assignReinforcements(p_map);
+        issueOrders(p_map);
+        executeOrders(p_map);
     }
 
-    public static void endGame() {
-        System.exit(0);
+    private static void assignReinforcements(Map p_map) {
+        // to assign reinforcements
+    }
+
+    private static void issueOrders(Map p_map) {
+        int l_playersLeftToIssueOrder = p_map.getD_players().size();
+        while (l_playersLeftToIssueOrder > 0) {
+            for (Player l_player : p_map.getD_players()) {
+                if (l_player.getD_reinforcements() != 0) {
+                    l_player.issue_order();
+                } else {
+                    l_playersLeftToIssueOrder--;
+                }
+            }
+        }
+    }
+
+    private static void executeOrders(Map p_map) {
+        int l_playersLeftToExecuteOrders = p_map.getD_players().size();
+        while (l_playersLeftToExecuteOrders > 0) {
+            for (Player l_player : p_map.getD_players()) {
+                if (!l_player.getD_orderList().isEmpty()) {
+                    l_player.next_order().execute();
+                } else {
+                    l_playersLeftToExecuteOrders--;
+                }
+            }
+        }
     }
 
     /**
@@ -118,5 +162,13 @@ public class GameEngine {
         }
 
         return !"-add".equals(commandArgs[commandArgs.length - 1]) && !"-remove".equals(commandArgs[commandArgs.length - 1]);
+    }
+
+    public Map getD_map() {
+        return d_map;
+    }
+
+    public static void endGame() {
+        System.exit(0);
     }
 }
