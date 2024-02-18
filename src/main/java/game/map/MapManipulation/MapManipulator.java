bffef5149f4e6320ebd74e924646f3724d885b7d
Joyjit Choudhury <joyjitc@users.noreply.github.com>
2024-02-17 22:25:44 -0500

    Added JavaDocs for MapManipulator

--- src/main/java/game/map/MapManipulation/MapManipulator.java
+++ src/main/java/game/map/MapManipulation/MapManipulator.java
@@ -4,20 +4,31 @@ import game.map.Map;
 
 import static game.constants.MapManipulation.*;
 
+/**
+ * The MapManipulator class is responsible for processing commands related to map manipulation. It
+ * delegates the processing of specific commands to corresponding processor classes.
+ */
 public class MapManipulator {
-    public void processCommand(String[] args, Map map) {
-        switch (args[0]) {
-            case EDIT_CONTINENT:
-                EditContinentProcessor.process(args, map);
-                break;
-            case EDIT_COUNTRY:
-                EditCountryProcessor.process(args, map);
-                break;
-            case EDIT_NEIGHBOR:
-                EditNeighborProcessor.process(args, map);
-                break;
-            default:
-                System.out.println("Invalid command please try again");
-        }
+
+  /**
+   * Processes the given command based on the arguments and the game map.
+   *
+   * @param args The command arguments.
+   * @param map The game map on which the command will be executed.
+   */
+  public void processCommand(String[] args, Map map) {
+    switch (args[0]) {
+      case EDIT_CONTINENT:
+        EditContinentProcessor.process(args, map);
+        break;
+      case EDIT_COUNTRY:
+        EditCountryProcessor.process(args, map);
+        break;
+      case EDIT_NEIGHBOR:
+        EditNeighborProcessor.process(args, map);
+        break;
+      default:
+        System.out.println("Invalid command please try again");
     }
+  }
 }
