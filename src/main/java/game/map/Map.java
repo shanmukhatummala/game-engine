e01094958a085f1e1fb0ac190deaf490e9036f6c
Joyjit Choudhury <joyjitc@users.noreply.github.com>
2024-02-18 15:20:49 -0500

    Resolved conflicts on Map.java etc

--- src/main/java/game/map/Map.java
+++ src/main/java/game/map/Map.java
@@ -21,11 +21,11 @@ import static java.util.stream.Collectors.toList;
 /**
  * Map is used for storing the details of the map
  * It contains the details of countries, continents and players associated with the map
+ * All manipulation of the Map is executed on an object of this class
  * @author Shanmukha
- * Java Model for the Game Map. All manipulation of the Map is executed on an object of this class
  */
 //@AllArgsConstructor
-@Data
+//@Data
 public class Map {
 
     private final List<Continent> d_continents;
@@ -56,48 +56,15 @@ public class Map {
      * @param p_continent the continent to be added
      * @throws IllegalArgumentException when a continent with same id already exists
      */
-    public void addContinent(Continent p_continent) {
-
-        for (Continent l_thisContinent : d_continents) {
-            if (l_thisContinent.getD_id() == p_continent.getD_id()) {
-                throw new IllegalArgumentException("Continent with same id already exists");
-            }
-        }
-
-        d_continents.add(p_continent);
-    }
-
-    /**
-     * <p>This method adds a country to the list of countries in the map</p>
-     * @param p_country the continent to be added
-     * @throws IllegalArgumentException when a country with same id already exists
-     */
-    public void addCountry(Country p_country) {
-
-        for (Country l_thisCountry : d_countries) {
-            if (l_thisCountry.getD_id() == p_country.getD_id()) {
-                throw new IllegalArgumentException("Country with same id already exists");
-            }
-        }
-        d_countries.add(p_country);
-    }
-
-
-  /**
-   * Adds a continent to the game map.
-   *
-   * @param continent The continent to be added.
-   * @throws IllegalArgumentException If a continent with the same ID already exists.
-   */
-  public void addContinent(Continent continent) {
+  public void addContinent(Continent p_continent) {
 
     for (Continent thisContinent : d_continents) {
-      if (thisContinent.getD_id() == continent.getD_id()) {
+      if (thisContinent.getD_id() == p_continent.getD_id()) {
         throw new IllegalArgumentException("Continent with same id already exists");
       }
     }
 
-      d_continents.add(continent);
+      d_continents.add(p_continent);
     System.out.println("Continent added successfully!");
   }
 
@@ -108,15 +75,15 @@ public class Map {
    */
   public void removeContinent(Integer p_continent_id) {
     List<Integer> l_linked_countries =
-        continents.stream()
+        d_continents.stream()
             .filter(Objects::nonNull)
-            .filter(continent -> continent.getId() == p_continent_id)
+            .filter(continent -> continent.getD_id() == p_continent_id)
             .findFirst()
-            .map(continent -> new ArrayList<>(continent.getCountryIdList()))
+            .map(continent -> new ArrayList<>(continent.getD_countryIdList()))
             .orElse(new ArrayList<>());
 
-    Boolean l_is_continent_removed =
-        continents.removeIf(continent -> continent.getId() == p_continent_id);
+    boolean l_is_continent_removed =
+            d_continents.removeIf(continent -> continent.getD_id() == p_continent_id);
 
     if (l_is_continent_removed) {
       l_linked_countries.forEach(this::removeCountry);
@@ -127,37 +94,70 @@ public class Map {
     }
   }
 
-  /**
-   * <p>This method adds a player to the list of players</p>
-   * @param p_playerName the player name to be added
-   * @throws IllegalArgumentException when a player with same name already exists
-   */
-  public void addPlayer(String p_playerName) {
+    /**
+     * <p>This method adds a country to the list of countries in the map</p>
+     * @param p_country the continent to be added
+     * @throws IllegalArgumentException when a country with same id already exists
+     */
+  public void addCountry(Country p_country) {
 
-    for (Player l_thisPlayer : d_players) {
-      if (l_thisPlayer.getD_name().equals(p_playerName)) {
-        throw new IllegalArgumentException("Player with same name already exists");
-      }
-    }
-    d_players.add(new Player(p_playerName));
-  }
-
-/**
-   * Adds a country to the game map.
-   *
-   * @param country The country to be added.
-   * @throws IllegalArgumentException If a country with the same ID already exists.
-   */
-  public void addCountry(Country country) {
-
-    for (Country thisCountry : countries) {
-      if (thisCountry.getId() == country.getId()) {
+    for (Country thisCountry : d_countries) {
+      if (thisCountry.getD_id() == p_country.getD_id()) {
         throw new IllegalArgumentException("Country with same id already exists");
       }
     }
-    countries.add(country);
+      d_countries.add(p_country);
     System.out.println("Country added successfully!");
   }
+
+    /**
+     * Removes a country from the game map.
+     *
+     * @param p_country_id The ID of the country to be removed.
+     */
+    public void removeCountry(Integer p_country_id) {
+        Integer l_linked_continent_id =
+                d_countries.stream()
+                        .filter(Objects::nonNull)
+                        .filter(country -> country.getD_id() == p_country_id)
+                        .findFirst()
+                        .map(Country::getD_continent)
+                        .map(Continent::getD_id)
+                        .orElse(null);
+
+        boolean l_is_country_removed = d_countries.removeIf(country -> country.getD_id() == p_country_id);
+
+        if (l_is_country_removed) {
+            d_countries.forEach(country -> country.removeNeighbor(p_country_id));
+
+            if (l_linked_continent_id != null) {
+                removeCountryFromContinent(l_linked_continent_id, p_country_id);
+            }
+
+            System.out.println("Country removed successfully!");
+        } else {
+            System.out.println("No Country with the given ID exists!");
+        }
+    }
+
+    public List<Player> getD_players() {
+        return d_players;
+    }
+
+    /**
+     * <p>This method adds a player to the list of players</p>
+     * @param p_playerName the player name to be added
+     * @throws IllegalArgumentException when a player with same name already exists
+     */
+    public void addPlayer(String p_playerName) {
+
+        for (Player l_thisPlayer : d_players) {
+            if (l_thisPlayer.getD_name().equals(p_playerName)) {
+                throw new IllegalArgumentException("Player with same name already exists");
+            }
+        }
+        d_players.add(new Player(p_playerName));
+    }
 
     /**
      * <p>This method removes a player from the list of players</p>
@@ -176,40 +176,23 @@ public class Map {
     }
 
 
-  /**
-   * Removes a country from the game map.
-   *
-   * @param p_country_id The ID of the country to be removed.
-   */
-  public void removeCountry(Integer p_country_id) {
-    Integer l_linked_continent_id =
-        countries.stream()
-            .filter(Objects::nonNull)
-            .filter(country -> country.getId() == p_country_id)
-            .findFirst()
-            .map(Country::getContinent)
-            .map(Continent::getId)
-            .orElse(null);
-
-    Boolean l_is_country_removed = countries.removeIf(country -> country.getId() == p_country_id);
-
-    if (l_is_country_removed) {
-      countries.forEach(country -> country.removeNeighbor(p_country_id));
-
-      if (l_linked_continent_id != null) {
-        removeCountryFromContinent(l_linked_continent_id, p_country_id);
-      }
-
-      System.out.println("Country removed successfully!");
-    } else {
-      System.out.println("No Country with the given ID exists!");
-    }
-  }
 
 
 
     public List<Country> getD_countries() {
         return d_countries;
+    }
+
+    /**
+     * Adds a country to a continent.
+     *
+     * @param p_continent_id The ID of the continent.
+     * @param p_country_id The ID of the country to be added to the continent.
+     */
+    public void addCountryToContinent(Integer p_continent_id, Integer p_country_id) {
+        d_continents.stream()
+                .filter(c -> c.getD_id() == p_continent_id)
+                .forEach(c -> c.getD_countryIdList().add(p_country_id));
     }
 
 
@@ -220,43 +203,10 @@ public class Map {
    * @param p_country_id The ID of the country to be removed from the continent.
    */
   public void removeCountryFromContinent(Integer p_continent_id, Integer p_country_id) {
-    continents.stream()
-        .filter(c -> c.getId() == p_continent_id)
-        .forEach(c -> c.getCountryIdList().removeIf(id -> Objects.equals(id, p_country_id)));
+    d_continents.stream()
+        .filter(c -> c.getD_id() == p_continent_id)
+        .forEach(c -> c.getD_countryIdList().removeIf(id -> Objects.equals(id, p_country_id)));
   }
-
-  /**
-   * Adds a country to a continent.
-   *
-   * @param p_continent_id The ID of the continent.
-   * @param p_country_id The ID of the country to be added to the continent.
-   */
-  public void addCountryToContinent(Integer p_continent_id, Integer p_country_id) {
-    continents.stream()
-        .filter(c -> c.getId() == p_continent_id)
-        .forEach(c -> c.getCountryIdList().add(p_country_id));
-  }
-
-  /**
-   * Adds a player to the game.
-   *
-   * @param playerName The name of the player to be added.
-   * @throws IllegalArgumentException If a player with the same name already exists.
-   */
-  public void addPlayer(String playerName) {
-
-    for (Player thisPlayer : players) {
-      if (thisPlayer.getName().equals(playerName)) {
-        throw new IllegalArgumentException("Player with same name already exists");
-      }
-    }
-    players.add(new Player(playerName));
-  }
-
-
-    public List<Player> getD_players() {
-        return d_players;
-    }
 
 
     /**
@@ -322,22 +272,6 @@ public class Map {
 
 
   /**
-   * Removes a player from the game.
-   *
-   * @param playerName The name of the player to be removed.
-   * @throws IllegalArgumentException If no player exists with the given name.
-   */
-  public void removePlayer(String playerName) {
-    for (Player thisPlayer : players) {
-      if (thisPlayer.getName().equals(playerName)) {
-        players.remove(thisPlayer);
-        return;
-      }
-    }
-    throw new IllegalArgumentException("No player exists with this name");
-  }
-
-  /**
    * Adds a neighbor to a country.
    *
    * @param p_country_id The ID of the country.
@@ -345,15 +279,15 @@ public class Map {
    */
   public void addNeighborToCountry(Integer p_country_id, Integer p_neighbor_country_id) {
     Optional<Country> l_country =
-        countries.stream()
+            d_countries.stream()
             .filter(Objects::nonNull)
-            .filter(c -> c.getId() == p_country_id)
+            .filter(c -> c.getD_id() == p_country_id)
             .findFirst();
 
     Optional<Country> l_neighbor_country =
-        countries.stream()
+            d_countries.stream()
             .filter(Objects::nonNull)
-            .filter(c -> c.getId() == p_neighbor_country_id)
+            .filter(c -> c.getD_id() == p_neighbor_country_id)
             .findFirst();
 
     if (l_country.isEmpty()) {
@@ -376,15 +310,15 @@ public class Map {
    */
   public void removeNeighborFromCountry(Integer p_country_id, Integer p_neighbor_country_id) {
     Optional<Country> l_country =
-        countries.stream()
+        d_countries.stream()
             .filter(Objects::nonNull)
-            .filter(c -> c.getId() == p_country_id)
+            .filter(c -> c.getD_id() == p_country_id)
             .findFirst();
 
     Optional<Country> l_neighbor_country =
-        countries.stream()
+        d_countries.stream()
             .filter(Objects::nonNull)
-            .filter(c -> c.getId() == p_neighbor_country_id)
+            .filter(c -> c.getD_id() == p_neighbor_country_id)
             .findFirst();
 
     if (l_country.isEmpty()) {
