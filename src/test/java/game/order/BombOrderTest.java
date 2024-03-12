36f1d2a701535e0f6a6f46d0ffe5a403e14d1616
shanmukhatummala <shanmukha.tummala@outlook.com>
2024-03-12 14:03:06 -0400

    add bomb order functionality

---
+++ src/test/java/game/order/BombOrderTest.java
@@ -0,0 +1,81 @@
+package game.order;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+
+import game.pojo.Continent;
+import game.pojo.Country;
+import game.pojo.Player;
+
+import org.junit.jupiter.api.Test;
+
+import pl.pojo.tester.api.assertion.Method;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** Test for BombOrder class */
+public class BombOrderTest {
+
+    /** Tests if constructor and getters are implemented properly */
+    @Test
+    public void shouldPassAllPojoTests() {
+        final Class<BombOrder> l_classUnderTest = BombOrder.class;
+        assertPojoMethodsFor(l_classUnderTest)
+                .testing(Method.CONSTRUCTOR, Method.GETTER)
+                .areWellImplemented();
+    }
+
+    /**
+     * Tests that the target is bombed when the target is adjacent to the initiator's countries and
+     * belongs to an opponent
+     */
+    @Test
+    public void shouldBombWhenTargetIsAdjacentAndBelongsToAnOpponent() {
+
+        Continent l_continent = new Continent();
+        Country l_country1 = new Country(1, "Country1", l_continent);
+        Country l_country2 = new Country(2, "Country2", l_continent, new ArrayList<>(), 10);
+        l_country1.addNeighbor(l_country2.getD_id());
+        Player l_player = new Player("Player", List.of(l_country1));
+        BombOrder l_bombOrder = new BombOrder(l_country2, l_player);
+
+        l_bombOrder.execute();
+
+        assertThat(l_country2.getD_armyCount(), equalTo(5));
+    }
+
+    /**
+     * Tests that the target is not bombed when the target is not adjacent to the initiator's
+     * countries
+     */
+    @Test
+    public void shouldNotBombWhenTargetIsNotAdjacent() {
+
+        Continent l_continent = new Continent();
+        Country l_country1 = new Country(1, "Country1", l_continent);
+        Country l_country2 = new Country(2, "Country2", l_continent, new ArrayList<>(), 10);
+        Player l_player = new Player("Player", List.of(l_country1));
+        BombOrder l_bombOrder = new BombOrder(l_country2, l_player);
+
+        l_bombOrder.execute();
+
+        assertThat(l_country2.getD_armyCount(), equalTo(10));
+    }
+
+    /** Tests that the target is not bombed when the target belongs to the initiator */
+    @Test
+    public void shouldNotBombWhenTargetBelongsToSamePlayer() {
+
+        Continent l_continent = new Continent();
+        Country l_country1 = new Country(1, "Country1", l_continent, new ArrayList<>(), 10);
+        Player l_player = new Player("Player", List.of(l_country1));
+        BombOrder l_bombOrder = new BombOrder(l_country1, l_player);
+
+        l_bombOrder.execute();
+
+        assertThat(l_country1.getD_armyCount(), equalTo(10));
+    }
+}
