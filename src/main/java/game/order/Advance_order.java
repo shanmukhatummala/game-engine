d81e864ce44e9589ab2d93cb4a112e1ae86f5bf2
rayapudi naveen <rayapudinaveen777@gmail.com>
2024-03-13 19:25:31 -0400

    -updated file AdvanceOrder2

--- src/main/java/game/order/Advance_order.java
+++ src/main/java/game/order/Advance_order.java
@@ -3,13 +3,16 @@ package game.order;
 import game.pojo.Country;
 import game.pojo.Player;
 
+import java.util.List;
+
+import static game.map.MapHelper.isAdjacent;
+
 public class Advance_order extends Order {
 
     private int d_armyCount;
 
     /**
      * This class extends from order class and represents the move order type of orders
-     *
      * @author Naveen
      */
     private Country destination;
@@ -29,6 +32,24 @@ public class Advance_order extends Order {
         this.armyNumber = armyNumber;
     }
 
+    /**
+     * Executes the Advance order, moving armies from the source territory to the destination territory
+     */
+
     @Override
-    public void execute() {}
+    public void execute() {
+        List<Country> l_countriesOfInitiator = this.getD_initiator().getD_countries();
+        if (destination.getD_name().equals(this.getD_initiator())) {
+            int armies = destination.getD_armyCount() + armyNumber;
+            System.out.println(armies);
+
+        } else {
+            if (!isAdjacent(l_countriesOfInitiator, destination)) {
+                attackTerritory(destination, armyNumber);
+            }
+        }
+    }
+
+    private void attackTerritory(Country target, int armyNumber) {}
+
 }
