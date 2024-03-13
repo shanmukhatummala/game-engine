d1234bc78b75b7c450b49cef63937be3a836a69f
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-03-13 19:53:21 -0400

    refactoring loadmap and showmap method in the phases

--- src/main/java/game/states/PlayPhase.java
+++ src/main/java/game/states/PlayPhase.java
@@ -1,8 +1,33 @@
 package game.states;
 
+
+import static game.map.MapEditor.editMap;
+import static game.map.MapHelper.playerOwnsContinent;
+import static game.map.MapLoader.loadMap;
+import static game.map.MapShower.showMap;
+import static game.map.MapValidator.isMapValid;
+import static game.util.FileHelper.createNewFileForMap;
+import static game.util.FileHelper.fileExists;
+
+import game.commands.Command;
+import game.commands.CommandParser;
+import game.map.Map;
+import game.pojo.Continent;
+import game.pojo.Country;
+import game.pojo.Player;
+import game.states.Phase;
+import lombok.Setter;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
 public class PlayPhase implements Phase{
     @Override
-    public void loadMapHandler() {
+    public void loadMapHandler(Command p_command, Map p_map) {
 
     }
 
@@ -17,7 +42,7 @@ public class PlayPhase implements Phase{
     }
 
     @Override
-    public void showMapHandler() {
+    public void showMapHandler(Map p_map) {
 
     }
 
--- src/main/java/game/states/StartUpPhase.java
+++ src/main/java/game/states/StartUpPhase.java
@@ -1,9 +1,42 @@
 package game.states;
 
-public class StartUpPhase implements Phase{
-    @Override
-    public void loadMapHandler() {
 
+
+
+import static game.map.MapEditor.editMap;
+import static game.map.MapHelper.playerOwnsContinent;
+import static game.map.MapLoader.loadMap;
+import static game.map.MapShower.showMap;
+import static game.map.MapValidator.isMapValid;
+import static game.util.FileHelper.createNewFileForMap;
+import static game.util.FileHelper.fileExists;
+
+import game.commands.Command;
+import game.commands.CommandParser;
+import game.map.Map;
+import game.pojo.Continent;
+import game.pojo.Country;
+import game.pojo.Player;
+import game.states.Phase;
+import lombok.Setter;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.ConcurrentMap;
+
+public class StartUpPhase implements Phase{
+    public static final String RESOURCES_PATH = "src/main/resources/";
+    @Override
+    public void loadMapHandler(Command p_command, Map p_map) {
+        loadMap(RESOURCES_PATH + p_command.getArgs().get(0), p_map);
+        if (!isMapValid(p_map)) {
+            System.out.println(
+                    "The loaded map is invalid, please load a valid map.");
+            p_map.clearMap();
+        }
     }
 
     @Override
@@ -17,8 +50,8 @@ public class StartUpPhase implements Phase{
     }
 
     @Override
-    public void showMapHandler() {
-
+    public void showMapHandler(Map p_map) {
+        showMap(p_map);
     }
 
     @Override
