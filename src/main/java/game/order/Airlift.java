dd6db37a6d5c6486af54cc66316f919b87a23acc
rayapudi naveen <rayapudinaveen777@gmail.com>
2024-03-16 20:58:24 -0400

    Airlift :code conventions

--- src/main/java/game/order/Airlift.java
+++ src/main/java/game/order/Airlift.java
@@ -8,37 +8,39 @@ import java.util.Random;
 
 /**
  * This class extends from order class and represents the Airlift
- *
  * @author Naveen
  */
+
 public class Airlift extends Order {
 
+
+    private Player d_destinationOwner;
+
+    private final Country d_destination;
+    private final Country d_source;
+    private final int d_armyNumber;
     /**
      * Constructor for Advance
      *
-     * @param initiator Player object who initiated the order
-     * @param destination Country object representing the destination territory
-     * @param destinationOwner Player object represents owner of the destination country
-     * @param source Country object representing the source territory
-     * @param armyNumber Integer representing the number of armies to move
+     * @param p_initiator Player object who initiated the order
+     * @param p_destination Country object representing the destination territory
+     * @param p_destinationOwner Player object represents owner of the destination country
+     * @param p_source Country object representing the source territory
+     * @param p_armyNumber Integer representing the number of armies to move
      */
-    private Player destinationOwner;
-
-    private final Country destination;
-    private final Country source;
-    private final int armyNumber;
-
     public Airlift(
-            Player initiator,
-            Player destinationOwner,
-            Country destination,
-            Country source,
-            int armyNumber) {
-        super(initiator);
-        this.destination = destination;
-        this.source = source;
-        this.armyNumber = armyNumber;
-        this.destinationOwner = destinationOwner;
+            Player p_initiator,
+            Player p_destinationOwner,
+            Country p_destination,
+            Country p_source,
+            int p_armyNumber) {
+
+
+        super(p_initiator);
+        this.d_destination = p_destination;
+        this.d_source = p_source;
+        this.d_armyNumber = p_armyNumber;
+        this.d_destinationOwner = p_destinationOwner;
     }
 
     /**
@@ -52,28 +54,28 @@ public class Airlift extends Order {
     public void execute() {
         if (valid()) {
             List<Country> l_countriesOfInitiator = this.getD_initiator().getD_countries();
-            if (l_countriesOfInitiator.contains(destination)) {
-                source.setD_armyCount(source.getD_armyCount() - armyNumber);
-                destination.setD_armyCount(destination.getD_armyCount() + armyNumber);
+            if (l_countriesOfInitiator.contains(d_destination)) {
+                d_source.setD_armyCount(d_source.getD_armyCount() - d_armyNumber);
+                d_destination.setD_armyCount(d_destination.getD_armyCount() + d_armyNumber);
             } else {
 
-                if (destinationOwner != null
+                if (d_destinationOwner != null
                         && (getD_initiator()
                                         .getD_negotiatedPlayers()
-                                        .contains(destinationOwner.getD_name())
-                                || destinationOwner
+                                        .contains(d_destinationOwner.getD_name())
+                                || d_destinationOwner
                                         .getD_negotiatedPlayers()
                                         .contains(getD_initiator().getD_name()))) {
                     System.out.println(
                             "Both players, "
-                                    + destinationOwner.getD_name()
+                                    + d_destinationOwner.getD_name()
                                     + " and "
                                     + getD_initiator().getD_name()
                                     + ", are under negotiation. So, cannot attack.");
                     return;
                 }
-                source.setD_armyCount(source.getD_armyCount() - armyNumber);
-                attackTerritory(destination, armyNumber, destinationOwner, getD_initiator());
+                d_source.setD_armyCount(d_source.getD_armyCount() - d_armyNumber);
+                attackTerritory(d_destination, d_armyNumber, d_destinationOwner, getD_initiator());
             }
         } else {
             System.out.println("Cannot Advance armies to the territory.");
@@ -91,55 +93,55 @@ public class Airlift extends Order {
     @Override
     public boolean valid() {
         List<Country> l_countriesOfInitiator = this.getD_initiator().getD_countries();
-        return l_countriesOfInitiator.contains(source);
+        return l_countriesOfInitiator.contains(d_source);
     }
 
     /**
-     * Simulates an attack on a target territory by simulating battles between attacking and
+     * Simulates an attack on a p_target territory by simulating battles between attacking and
      * defending armies. This method uses a random number generator to simulate the outcome of
      * battles between the attacking and defending armies. Each attacking army has a 60% chance of
      * killing one defending army, and each defending army has a 70% chance of killing one attacking
      * army. The battle continues until either all attacking or defending armies are eliminated.
      *
-     * @param target The target {@link Country} to attack. This country represents the defending
+     * @param p_target The p_target {@link Country} to attack. This country represents the defending
      *     territory.
-     * @param armyNumber The number of armies to use in the attack. This represents the attacking
+     * @param p_armyNumber The number of armies to use in the attack. This represents the attacking
      *     force.
-     * @param destinationOwner Player object represents owner of the destination country
-     * @param initiator Player object who initiated the order
+     * @param p_destinationOwner Player object represents owner of the destination country
+     * @param p_initiator Player object who initiated the order
      * @see Country#getD_armyCount()
      * @see Player#getD_countries()
      */
     public void attackTerritory(
-            Country target, int armyNumber, Player destinationOwner, Player initiator) {
-        Random random = new Random();
-        int attackingArmyCount = armyNumber;
-        int defendingArmyCount = target.getD_armyCount();
+            Country p_target, int p_armyNumber, Player p_destinationOwner, Player p_initiator) {
+        Random l_random = new Random();
+        int l_attackingArmyCount = p_armyNumber;
+        int l_defendingArmyCount = p_target.getD_armyCount();
 
-        while (attackingArmyCount > 0 && defendingArmyCount > 0) {
+        while (l_attackingArmyCount > 0 && l_defendingArmyCount > 0) {
             // Each attacking army has 60% chance of killing one defending army
-            for (int i = 0; i < attackingArmyCount; i++) {
-                if (random.nextDouble() <= 0.6) {
-                    defendingArmyCount--;
+            for (int i = 0; i < l_attackingArmyCount; i++) {
+                if (l_random.nextDouble() <= 0.6) {
+                    l_defendingArmyCount--;
                 }
             }
 
             // Each defending army has 70% chance of killing one attacking army
-            for (int i = 0; i < defendingArmyCount; i++) {
-                if (random.nextDouble() <= 0.7) {
-                    attackingArmyCount--;
+            for (int i = 0; i < l_defendingArmyCount; i++) {
+                if (l_random.nextDouble() <= 0.7) {
+                    l_attackingArmyCount--;
                 }
             }
         }
         // Update territory armies based on the outcome of the attack
-        if (defendingArmyCount <= 0) {
+        if (l_defendingArmyCount <= 0) {
             // Attacker wins
-            target.setD_armyCount(attackingArmyCount);
-            initiator.getD_countries().add(target);
-            destinationOwner.getD_countries().remove(target);
+            p_target.setD_armyCount(l_attackingArmyCount);
+            p_initiator.getD_countries().add(p_target);
+            p_destinationOwner.getD_countries().remove(p_target);
         } else {
             // Defender wins
-            target.setD_armyCount(defendingArmyCount);
+            p_target.setD_armyCount(l_defendingArmyCount);
         }
     }
 }
