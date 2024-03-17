073d78ba589888e4d34b676cdda8405a008b08c0
Joyjit Choudhury <joyjitc@users.noreply.github.com>
2024-03-16 21:51:21 -0400

    Added JavaDoc for Logger classes

--- src/main/java/game/logger/LogEntryWriter.java
+++ src/main/java/game/logger/LogEntryWriter.java
@@ -6,11 +6,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This class is responsible for writing log entries to a file. It implements the Observer interface to observe changes
+ * in a LogEntryBuffer and write new log entries to the specified file.
+ */
 @RequiredArgsConstructor
 public class LogEntryWriter implements Observer {
 
+    /**
+     * The name of the file to which log entries will be written.
+     */
     private final String d_fileName;
 
+    /**
+     * Updates the writer with new log entries from the observable LogEntryBuffer.
+     *
+     * @param p_observable The observable LogEntryBuffer that has been updated.
+     */
     public void update(Observable p_observable) {
         try {
             FileWriter l_fileWriter = new FileWriter(d_fileName, true);
--- src/main/java/game/logger/Observer.java
+++ src/main/java/game/logger/Observer.java
@@ -1,5 +1,15 @@
 package game.logger;
 
+/**
+ * This interface represents an observer in the observer pattern.
+ * It defines a single method, update, which is called by an Observable object
+ * when it has been updated.
+ */
 public interface Observer {
+    /**
+     * This method is called by an Observable object when it has been updated.
+     *
+     * @param p_observable The Observable object that has been updated.
+     */
     public void update(Observable p_observable);
 }
