d11360542594a8fbf64656d44b0ba503cf6d4d2a
shanmukhatummala <shanmukha.tummala@outlook.com>
2024-04-01 12:58:22 -0400

    add conquest file writer

--- src/main/java/game/mapfile/writer/ConquestFileWriter.java
+++ src/main/java/game/mapfile/writer/ConquestFileWriter.java
@@ -1,5 +1,8 @@
 package game.mapfile.writer;
 
+import static game.map.MapHelper.getCountryById;
+import static game.map.MapHelper.getCountryByName;
+
 import game.map.Map;
 import game.pojo.Continent;
 import game.pojo.Country;
@@ -7,6 +10,7 @@ import game.pojo.Country;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ConquestFileWriter {
     public void writeConquestFile(String p_path, Map p_map) {
@@ -16,8 +20,6 @@ public class ConquestFileWriter {
             writeContinents(l_writer, p_map);
             // write the countries to the file
             writeCountries(l_writer, p_map);
-            // write the border to the file
-            writeBorders(l_writer, p_map);
             // close the writer
             l_writer.close();
             System.out.println("Saved the map file");
@@ -32,14 +34,14 @@ public class ConquestFileWriter {
      *
      * @param p_writer file writer object
      * @param p_map map object
-     * @throws IOException
+     * @throws IOException when writer fails to write
      */
     private void writeContinents(BufferedWriter p_writer, Map p_map) throws IOException {
-        String l_continentsStarter = "[continents]";
+        String l_continentsStarter = "[Continents]";
         p_writer.write(l_continentsStarter);
         p_writer.newLine();
         for (Continent l_continent : p_map.getD_continents()) {
-            p_writer.write(l_continent.getD_name() + " " + l_continent.getD_bonus());
+            p_writer.write(l_continent.getD_name() + "=" + l_continent.getD_bonus());
             p_writer.newLine();
         }
         p_writer.newLine();
@@ -51,41 +53,16 @@ public class ConquestFileWriter {
      *
      * @param p_writer file writer object
      * @param p_map map object
-     * @throws IOException when writer fails to write then exception is thrown
+     * @throws IOException when writer fails to write
      */
     private void writeCountries(BufferedWriter p_writer, Map p_map) throws IOException {
-        String l_countryStarter = "[countries]";
+        String l_countryStarter = "[Territories]";
         p_writer.write(l_countryStarter);
         p_writer.newLine();
         for (Country l_country : p_map.getD_countries()) {
-            p_writer.write(
-                    l_country.getD_id()
-                            + " "
-                            + l_country.getD_name()
-                            + " "
-                            + l_country.getD_continent().getD_id());
-            p_writer.newLine();
-        }
-        p_writer.newLine();
-    }
-
-    /**
-     * this method is called in the saveMap method to write all the border of all countries as in a
-     * country and it's neighbors
-     *
-     * @param p_writer file writer object
-     * @param p_map map object
-     * @throws IOException when writer fails to write then exception is thrown
-     */
-    private void writeBorders(BufferedWriter p_writer, Map p_map) throws IOException {
-        String l_borderStarter = "[borders]";
-        p_writer.write(l_borderStarter);
-        p_writer.newLine();
-        for (Country l_country : p_map.getD_countries()) {
-            p_writer.write(Integer.toString(l_country.getD_id()));
-            for (Integer l_neighboursID : l_country.getD_neighborIdList()) {
-                p_writer.write(" " + l_neighboursID);
-            }
+            AtomicReference<String> l_countryRow = new AtomicReference<>(l_country.getD_name() + ",,," + l_country.getD_continent().getD_name());
+            l_country.getD_neighborIdList().forEach(neighborId -> l_countryRow.set(l_countryRow + "," + getCountryById(p_map, neighborId).getD_name()));
+            p_writer.write(l_countryRow.toString());
             p_writer.newLine();
         }
     }
