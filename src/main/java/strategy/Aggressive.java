4441a2d3545efe61829f197149d929de7c415f76
rayapudi naveen <rayapudinaveen777@gmail.com>
2024-04-05 16:53:37 -0400

    updated Aggressive

--- src/main/java/strategy/Aggressive.java
+++ src/main/java/strategy/Aggressive.java
@@ -1,104 +1,93 @@
 package strategy;
 
 import game.commands.Command;
+import game.commands.CommandParser;
 import game.map.Map;
 import game.pojo.Country;
 import game.pojo.Player;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import static game.map.MapHelper.getCountryById;
+import static game.map.MapHelper.getCountryOwner;
 
-public  class Aggressive extends PlayerStrategy {
+public class Aggressive extends PlayerStrategy {
 
-    private boolean Deployed;
-    private boolean Attacked;
-    private boolean Moved;
+    private boolean d_deployed = false;
+    private boolean d_attacked = false;
+    private boolean d_moved = false;
 
-    private static Aggressive aggressiveStrategy;
+    private static Aggressive d_aggressiveStrategy;
 
     public static Aggressive getAggressiveStrategy() {
-        if (aggressiveStrategy == null) {
-            aggressiveStrategy = new Aggressive() {};
+        if (d_aggressiveStrategy == null) {
+            Aggressive.d_aggressiveStrategy = new Aggressive();
         }
-        return aggressiveStrategy;
+        return d_aggressiveStrategy;
     }
 
-    /** Private constructor to make sure that the client calls getAggressiveStrategy() */
-    private Aggressive() {
-        Deployed = false;
-        Attacked = false;
-        Moved = false;
-    }
+    private Aggressive() {}
 
     @Override
     public Command createOrder(Map p_map, Player p_player) {
-        if (!Deployed) {
-            deployStrongestCountry(p_player);
-            Deployed = true;
-            return new Command("deploy");
-        } else if (!Attacked) {
-            attackWithStrongestCountry(p_map, p_player);
-            Attacked = true;
-            return new Command("attack");
-        } else if (!Moved) {
-            moveArmies(p_map, p_player);
-            Moved = true;
-            return new Command("move");
+        if (!d_deployed) {
+            d_deployed = true;
+            return deployCommandOnStrongest(p_player);
+        } else if (!d_attacked) {
+            d_attacked = true;
+            return attackCommandOnStrongest(p_map, p_player);
+        } else if (!d_moved) {
+            d_moved = true;
+            return moveCommandToCentralize(p_map, p_player);
         } else {
-            // If all actions are completed, reset for the next turn
-            Deployed = false;
-            Attacked = false;
-            Moved = false;
-            // No specific command, as turn is completed
-            return new Command("end_turn");
-        }
-
-        }
-
-        public void deployStrongestCountry(Player p_player) {
-            Country strongestCountry = findStrongestCountry(p_player.getD_countries());
-            int reinforcements = p_player.getD_reinforcements();
-            strongestCountry.setD_armyCount(strongestCountry.getD_armyCount() + reinforcements);
-            p_player.setD_reinforcements(0);
-        }
-
-    public void attackWithStrongestCountry(Map p_map,Player p_player ) {
-        Country strongestCountry = findStrongestCountry(p_player.getD_countries());
-        Set<Integer> neighborIds = strongestCountry.getD_neighborIdList();
-
-        for (Integer neighborId : neighborIds) {
-            Country neighbor = getCountryById(p_map, neighborId);
-            if (neighbor != null && strongestCountry.getD_armyCount() > neighbor.getD_armyCount()) {
-                int armiesToAttackWith = strongestCountry.getD_armyCount() - neighbor.getD_armyCount();
-                neighbor.setD_armyCount(0); // Attack and conquer neighbor
-                strongestCountry.setD_armyCount(armiesToAttackWith);
-                break; // Stop after one successful attack
-            }
-        }
-
-    }
-
-    public void moveArmies(Map p_map,Player p_player) {
-        Country strongestCountry = findStrongestCountry(p_player.getD_countries());
-        List<Integer> neighbors = new ArrayList<>(strongestCountry.getD_neighborIdList());
-        for (int neighborId : neighbors) {
-            Country neighbor = getCountryById(p_map, neighborId);
-            if (neighbor != null && neighbor.getD_continent().equals(p_player)) {
-                int totalArmies = strongestCountry.getD_armyCount() + neighbor.getD_armyCount();
-                neighbor.setD_armyCount(totalArmies);
-                strongestCountry.setD_armyCount(0);
-                break; // Move armies to the first neighboring country in the same continent
-            }
+            // Reset flags for the next round
+            d_deployed = false;
+            d_attacked = false;
+            d_moved = false;
+            return new Command("commit");
         }
     }
 
+    private Command deployCommandOnStrongest(Player p_player) {
+        String l_countryName = findStrongestCountry(p_player.getD_countries()).getD_name();
+        int l_reinforcementsToDeploy = p_player.getD_reinforcements();
+        return CommandParser.parse("deploy " + l_countryName + " " + l_reinforcementsToDeploy)
+                .get(0);
+    }
 
-
-    private Country findStrongestCountry(List<Country> countries) {
-            return Collections.max(countries, Comparator.comparingInt(Country::getD_armyCount));
+    private Command attackCommandOnStrongest(Map p_map, Player p_player) {
+        Country l_strongestCountry = findStrongestCountry(p_player.getD_countries());
+        Country l_targetCountry = findStrongestCountry(p_map.getD_countries());
+        if (l_strongestCountry.equals(l_targetCountry)) {
+            // If the strongest country is already the target, skip attacking
+            return new Command("commit");
         }
+        return CommandParser.parse("attack " + l_strongestCountry.getD_name() + " " + l_targetCountry.getD_name())
+                .get(0);
+    }
+
+    private Command moveCommandToCentralize(Map p_map, Player p_player) {
+        String l_targetCountryName;
+        Country l_strongestCountry = findStrongestCountry(p_player.getD_countries());
+        List<Integer> l_neighbors = new ArrayList<>(l_strongestCountry.getD_neighborIdList());
+        for (int l_neighborId : l_neighbors) {
+            Country l_neighbor = getCountryById(p_map, l_neighborId);
+            if (l_neighbor != null && getCountryOwner(l_neighbor, Collections.singletonList(p_player)).equals(p_player)) {
+                int totalArmies = l_strongestCountry.getD_armyCount() + l_neighbor.getD_armyCount();
+                l_targetCountryName =l_neighbor.getD_name();
+                return CommandParser.parse("move " + l_strongestCountry.getD_name() + " " + l_targetCountryName+ " "+ totalArmies)
+                        .get(0);
+            }
+        }
+        // Placeholder for handling the case where no move is made
+        return new Command("commit");
+
+    }
+
+    private Country findStrongestCountry(List<Country> p_countries) {
+        return Collections.max(p_countries, Comparator.comparingInt(Country::getD_armyCount));
+    }
 }
-
-
-
