ec654d43854044ebbb7c02d3b6190c15d8b856e7
Joyjit Choudhury <joyjitc@users.noreply.github.com>
2024-04-09 14:59:50 -0400

    Add handleTournament support to EditMapPhase

--- src/main/java/game/states/EditMapPhase.java
+++ src/main/java/game/states/EditMapPhase.java
@@ -163,4 +163,20 @@ public class EditMapPhase extends StartUpPhase {
                         + " you can't add or remove player while editing a map";
         printInvalidCommandMessage(message);
     }
+
+    /**
+     * Handles the tournament command. Displays an invalid command message if
+     * the command is not allowed in the phase
+     *
+     * @param p_commandList The list of commands related to the tournament
+     * @param p_map The current map.
+     */
+    @Override
+    public void handleTournament(List<Command> p_commandList, Map p_map) {
+        String l_message =
+                "Invalid Command in state "
+                        + this.getClass().getSimpleName()
+                        + " you can't start a tournament in this phase";
+        printInvalidCommandMessage(l_message);
+    }
 }
